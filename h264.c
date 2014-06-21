15a65349ea5c80ef6adc6455b8fefef8dd3f1ec4
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2014-06-21 14:18:26 +0200

    fixed comments

--- h264.c
+++ h264.c
@@ -62,7 +62,7 @@ AWB (auto white balance) algorithms.
 #define CAM_CONTRAST 0 //-100 .. 100
 #define CAM_BRIGHTNESS 50 //0 .. 100
 #define CAM_SATURATION 0 //-100 .. 100
-//100*(1/8) = 125
+//1000*(1/8) = 125
 #define CAM_SHUTTER_SPEED 125 //0 ..
 #define CAM_SHUTTER_SPEED_AUTO OMX_FALSE
 #define CAM_ISO 100 //100 .. 800
